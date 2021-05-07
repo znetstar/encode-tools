a229930ba796770267df32ee12c1fe1d449679e8
Zachary Boyd <zachary@zacharyboyd.nyc>
2021-05-07 17:01:46 -0400

    spins off encode tools into a new repo

--- src/EncodeToolsNative.ts
+++ src/EncodeToolsNative.ts
@@ -9,6 +9,16 @@ import EncodeTools, {
     SerializationFormat
 } from './EncodeTools';
 
+export {
+  BinaryEncoding,
+  BinaryInputOutput,
+  EncodingOptions as BaseEncodingOptions,
+  HashAlgorithm,
+  IDFormat,
+  SerializationFormat
+} from './EncodeTools';
+
+
 import * as crypto from 'crypto';
 
 
